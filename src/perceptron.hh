039ff32c45f83799be50b62308ce519768f3d35d
Azael Reyes <azael.devel@gmail.com>
2021-09-23 13:17:31 -0700

    saving..
    Signed-off-by: Azael Reyes <azael.devel@gmail.com>

--- src/perceptron.hh
+++ src/perceptron.hh
@@ -38,6 +38,9 @@ namespace oct::neu
 				if(randS > 0.5) weight[i] = core::randNumber(0.0,0.5);
 				else weight[i] = core::randNumber(0.0,0.5) * -1.0;
 			}
+			double randS = core::randNumber();
+			if(randS > 0.5) theta = core::randNumber(0.0,0.5);
+			else theta = core::randNumber(0.0,0.5) * -1.0;
 		}
 		
 		std::vector<T*>& get_inputs()
@@ -68,9 +71,8 @@ namespace oct::neu
 
 		T spread(ActivationFuntion AF)
 		{
-			//std::cout << "\tvoid datatype Perceptron::spread(datatype (*F)(datatype)) step 1\n";
 			sigma = sum();
-			//std::cout << "\tvoid datatype Perceptron::spread(datatype (*F)(datatype)) step 2\n";
+
 			switch(AF)
 			{
 				case ActivationFuntion::SIGMOIDEA:
@@ -79,11 +81,13 @@ namespace oct::neu
 				case ActivationFuntion::IDENTITY:
 					out =  sigma;
 				break;
+				case ActivationFuntion::SCALON:
+					out =  scalon(sigma);
+				break;
 				default:
 					throw oct::core::Exception("Funcion de activacion desconocida",__FILE__,__LINE__);
 			};
 			
-			//std::cout << "\tvoid datatype Perceptron::spread(datatype (*F)(datatype)) step 3\n";
 			return out;
 		}
 		void resize(unsigned short ins)
@@ -93,51 +97,19 @@ namespace oct::neu
 		}
 		T sum()
 		{
-			//std::cout << "\tdatatype Perceptron::sigma step 1\n";
 			T val = 0;
-			//std::cout << "\tdatatype Perceptron::sigma step 2\n";
+
 			if(inputs.size() != weight.size()) throw oct::core::Exception("Los tamanos de los vectores involucrados no son iguales",__FILE__,__LINE__);
-			//std::cout << "\tdatatype Perceptron::sigma step 3\n";
 			
 			for(unsigned short i = 0; i < inputs.size(); i++)
 			{
-				//std::cout << "\tdatatype Perceptron::sigma step 3.1 - " << i << "\n";
 				val += (*inputs[i]) * weight[i];
 			}
-			
-			//std::cout << "\tdatatype Perceptron::sigma step 4\n";
+			val += theta;
+
 			return val;
 		}
-		/**
-		*\brief Gradient Stteping
-		*/
-		void gd(T ratio, T dEdW)
-		{
-			//std::cout << "weight : ";
-			//print(weight);
-			/*if(dEdW > 0)
-			{
-				for(Index i = 0; i < weight.size(); i++)
-				{
-					weight[i] = weight[i]  - (dEdW * ratio);
-				}
-			}
-			else if(dEdW < 0)
-			{
-				for(Index i = 0; i < weight.size(); i++)
-				{
-					weight[i] = weight[i]  + (dEdW * ratio);
-				}
-			}*/
-			for(Index i = 0; i < weight.size(); i++)
-			{
-				weight[i] = weight[i]  - (dEdW * ratio);
-			}
 
-			//std::cout << " -> ";
-			//print(weight);
-			//std::cout << "\n";
-		}
 
 		static T sigmoide(T v)
 		{
@@ -155,21 +127,24 @@ namespace oct::neu
 		{
 			return T(1);
 		}
+		static T scalon(T v)
+		{
+			if(v > 0.5)return T(1);
+			else  return T(0);
+		}
 
 		void bp(const std::vector<Data<T>>& datas, const Learning<T>& learning,oct::math::Plotter* plotting)
 		{
 			
 		}
-	//private:
+
 		std::vector<T*> inputs;
 		std::vector<T> weight;
 		T out;
 		//T umbral;
 		//T sesgo;
 		T sigma;
-
-	private:
-		
+		T theta;
 		
 	};
 
